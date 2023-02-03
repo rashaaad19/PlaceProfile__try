66ddba081469a7f4091071f5dde66855e7dd6566
Youssef Rashad <egyoussef_2000@hotmail.com>
2023-02-03 21:08:11 +0200

    added slider section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import PlaceCard from './components/placeInfo/PlaceCard';
 import PlaceImage from './components/placeInfo/PlaceImage';
+import SliderContainer from './components/Slider/SliderContainer';
 function App() {
   return (
     <>
@@ -10,6 +11,9 @@ function App() {
     <div id='first__part'>
     <PlaceCard></PlaceCard>
     <PlaceImage></PlaceImage>
+    </div>
+    <div id='second__part'>
+    <SliderContainer></SliderContainer>
     </div>
   </div>
     </>
---
+++ src/components/Slider/SliderContainer.js
@@ -0,0 +1,8 @@
+import NearbySlider from './NearbySlider'
+import './SliderContainer.css'
+const SliderContainer=()=>{
+    return <>
+    <NearbySlider></NearbySlider>
+    </>
+}
+export default SliderContainer
